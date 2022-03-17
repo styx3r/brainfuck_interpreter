a2ca5c612819402643971955d31cf2484f8096be
styx3r <styxer@tutanota.com>
2022-03-17 18:49:49 +0100

    chaning test files path

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,31 @@ mod tests {
     use crate::interpreter::Interpreter;
     #[test]
     fn it_works() {
-        let path = "/home/styxer/GIT/brainfuck_interpreter/test_files/simple_hello_world.bf";
-        assert!(Interpreter::load_file(path).is_ok());
+        let path = "test_files/simple_hello.bf";
+        assert!(Interpreter::load_file(path).unwrap() == "Hello\n");
+    }
+
+    //-------------------------------------------------------------------------
+
+    #[test]
+    fn count_nine() {
+        let path = "test_files/count_nine.bf";
+        assert!(Interpreter::load_file(path).unwrap() == "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n");
+    }
+
+    //-------------------------------------------------------------------------
+
+    #[test]
+    fn wiki_hello_world() {
+        let path = "test_files/wiki_hello_world.bf";
+        assert!(Interpreter::load_file(path).unwrap() == "Hello World!\n");
+    }
+
+    //-------------------------------------------------------------------------
+
+    #[test]
+    fn loop_test() {
+        let path = "test_files/loop_test.bf";
+        assert!(Interpreter::load_file(path).unwrap().len() == 0);
     }
 }
